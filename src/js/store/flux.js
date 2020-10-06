02c8ce1613b1db7fec4c7415d96f1734ff2afc89
jdglez91 <jdglez1991@gmail.com>
2020-10-06 19:13:14 +0000

    incomplete

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,9 +1,29 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
+			contacts: []
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
+			loadContacts: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/JoseDavid_GonzalezGarcia")
+					.then(function(response) {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						// Read the response as json.
+						return response.json();
+					})
+					.then(function(responseAsJson) {
+						// Do stuff with the JSON
+						console.log("responseAsJson", responseAsJson);
+						setStore({ contacts: responseAsJson });
+						//.results specifies the location of our array inside of the fetch object in our API - same in line 30
+					})
+					.catch(function(error) {
+						console.log("Looks like there was a problem: \n", error);
+					});
+			}
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
